ecd49ced91f097090f4d72717f797420641eef61
Christopher Verbsky <chrisverbsky@gmail.com>
2023-09-29 13:04:56 -0500

    Step 7

---
+++ app/src/main/java/com/cs407/lab3_milestone1/ExampleDialog.java
@@ -0,0 +1,25 @@
+package com.cs407.lab3_milestone1;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class ExampleDialog extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Information")
+                .setMessage("This is a Dialog")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
